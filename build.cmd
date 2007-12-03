07f972293602060b97e68cb741f5d21a5bc58f79
Kirit Sælensminde <k@kirit.com>
2007-12-03 04:50:27 +0000

    Changed to new install path.

--- build.cmd
+++ build.cmd
@@ -1,6 +1,8 @@
+call "c:\Program Files\Microsoft Visual Studio 8\VC\bin\vcvars32.bat"
 cd src\tools\jam
 call build_dist.bat
 copy boost-jam-3.1.16-1-ntx86\bjam.exe ..\..\..
 cd ..\..\..
 bjam --toolset=msvc-8.1 tools
-bjam --toolset=msvc-8.1 --prefix=..\install\basic debug release runtime-link=shared threading=multi install --without-python
+bjam --toolset=msvc-8.1 --prefix=..\install debug release runtime-link=shared threading=multi install --without-python
+cd ..
