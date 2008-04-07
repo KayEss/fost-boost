88746c1d3f00ddb760f31d8bb583c16cf0e14820
Kirit Sælensminde <k@kirit.com>
2008-04-07 09:58:27 +0000

    Changed build options to include Python and to use multiple threads.

--- build.cmd
+++ build.cmd
@@ -6,5 +6,5 @@ call build_dist.bat
 copy boost-jam-3.1.16-1-ntx86\bjam.exe ..\..\..
 cd ..\..\..
 bjam --toolset=msvc-8.0 tools
-bjam --toolset=msvc-8.0 --prefix=..\install debug release runtime-link=shared threading=multi install --without-python
+bjam -jx 3 --toolset=msvc-8.0 --prefix=..\install debug release runtime-link=shared threading=multi install
 cd ..
