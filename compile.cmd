b08ada1044ddc652843f2084877fc2de8ae88c00
Kirit Sælensminde <k@kirit.com>
2008-04-30 02:28:50 +0000

    Changed compile options. These will need to change for the main projects too.

--- compile.cmd
+++ compile.cmd
@@ -3,5 +3,5 @@ call build_dist.bat
 copy boost-jam-3.1.16-1-ntx86\bjam.exe ..\..\..
 cd ..\..\..
 bjam --toolset=msvc-8.0 tools
-bjam -j3 --toolset=msvc-8.0 --prefix=..\install debug release runtime-link=shared threading=multi install
+bjam -j3 --toolset=msvc-8.0 --prefix=..\install define=_SECURE_SCL=0 define=_CRT_SECURE_NO_DEPRECATE debug release runtime-link=shared threading=multi install
 cd ..
