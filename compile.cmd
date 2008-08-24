5d9590e7372f6133fdb4462a9eaeaf5d1aa8be66
Kirit Sælensminde <k@kirit.com>
2008-08-24 09:35:01 +0000

    Changes to compile and install instructions so that Boost files are installed locally.

--- compile.cmd
+++ compile.cmd
@@ -2,6 +2,6 @@ cd src\tools\jam
 call build_dist.bat
 copy boost-jam-3.1.16-1-ntx86\bjam.exe ..\..\..
 cd ..\..\..
-bjam --toolset=msvc-8.0 tools
-bjam -j3 --toolset=msvc-8.0 --prefix=..\install define=_SECURE_SCL=0 define=_CRT_SECURE_NO_DEPRECATE debug release runtime-link=shared threading=multi install
+bjam -j3 --toolset=msvc-8.0 define=_SECURE_SCL=0 define=_CRT_SECURE_NO_DEPRECATE define=_CRT_SECURE_NO_WARNINGS tools
+bjam -j3 --toolset=msvc-8.0 define=_SECURE_SCL=0 define=_CRT_SECURE_NO_DEPRECATE define=_CRT_SECURE_NO_WARNINGS debug release runtime-link=shared threading=multi --prefix=..\install install
 cd ..
