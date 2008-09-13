cee8454d64a2dc03a83d965808c5c8adb866b805
Kirit Sælensminde <k@kirit.com>
2008-09-13 09:28:19 +0000

    Updated to add in exception handling options.

--- compile.cmd
+++ compile.cmd
@@ -2,6 +2,6 @@ cd src\tools\jam
 call build_dist.bat
 copy boost-jam-3.1.16-1-ntx86\bjam.exe ..\..\..
 cd ..\..\..
-bjam -j3 --toolset=msvc-8.0 define=_SECURE_SCL=0 define=_CRT_SECURE_NO_DEPRECATE define=_CRT_SECURE_NO_WARNINGS tools --asynch-exceptions=on --exception-handling=on --extern-c-nothrow=off
-bjam -j3 --toolset=msvc-8.0 define=_SECURE_SCL=0 define=_CRT_SECURE_NO_DEPRECATE define=_CRT_SECURE_NO_WARNINGS debug release runtime-link=shared threading=multi --prefix=..\install --asynch-exceptions=on --exception-handling=on --extern-c-nothrow=off install
+bjam -j3 --toolset=msvc-8.0 define=_SECURE_SCL=0 define=_CRT_SECURE_NO_DEPRECATE define=_CRT_SECURE_NO_WARNINGS tools asynch-exceptions=on exception-handling=on extern-c-nothrow=off
+bjam -j3 --toolset=msvc-8.0 define=_SECURE_SCL=0 define=_CRT_SECURE_NO_DEPRECATE define=_CRT_SECURE_NO_WARNINGS debug release install runtime-link=shared threading=multi asynch-exceptions=on exception-handling=on extern-c-nothrow=off --prefix=..\install
 cd ..
