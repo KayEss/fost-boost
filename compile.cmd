fd470a3715e0e5fa532ec8df51ea9f2188e3287b
Kirit Sælensminde <k@kirit.com>
2008-11-09 11:35:24 +0000

    Final tweak to the Windows build instructions to remove the MPI warning.

--- compile.cmd
+++ compile.cmd
@@ -1,4 +1,4 @@
 cd %VERSION%
-bjam -j%NUMBER_OF_PROCESSORS% --toolset=msvc-8.0 define=_SECURE_SCL=0 define=_CRT_SECURE_NO_DEPRECATE define=_CRT_SECURE_NO_WARNINGS tools asynch-exceptions=on exception-handling=on extern-c-nothrow=off
-bjam -j%NUMBER_OF_PROCESSORS% --toolset=msvc-8.0 define=_SECURE_SCL=0 define=_CRT_SECURE_NO_DEPRECATE define=_CRT_SECURE_NO_WARNINGS debug release install runtime-link=shared threading=multi asynch-exceptions=on exception-handling=on extern-c-nothrow=off --prefix=..\boost
+bjam -j%NUMBER_OF_PROCESSORS% toolset=msvc-8.0 define=_SECURE_SCL=0 define=_CRT_SECURE_NO_DEPRECATE define=_CRT_SECURE_NO_WARNINGS --without-mpi tools asynch-exceptions=on exception-handling=on extern-c-nothrow=off
+bjam -j%NUMBER_OF_PROCESSORS% toolset=msvc-8.0 define=_SECURE_SCL=0 define=_CRT_SECURE_NO_DEPRECATE define=_CRT_SECURE_NO_WARNINGS --without-mpi debug release install runtime-link=shared threading=multi asynch-exceptions=on exception-handling=on extern-c-nothrow=off --prefix=..\boost
 cd ..
