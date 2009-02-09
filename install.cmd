3e4d66642a22cafad48ebdfb3875a9724381f5bb
Kirit Sælensminde <k@kirit.com>
2009-02-09 03:58:56 +0000

    Updated installer so that it creates the new build directory entry as well.

--- install.cmd
+++ install.cmd
@@ -8,6 +8,7 @@ cmd /A /C echo BOOST_BUILD ?= Boost/1_%1_%2/tools/build/v2 ;>>..\boost-build.jam
 cmd /A /C echo boost-build $(BOOST_BUILD) ;>>..\boost-build.jam
 
 del ..\boost-version.jam
-cmd /A /C echo BOOST_VERSION_MAJOR = "%1" ;>> ../boost-version.jam
-cmd /A /C echo BOOST_VERSION_MINOR = "%2" ;>> ../boost-version.jam
-cmd /A /C echo BOOST_HEADERS = "../Boost/boost/include/boost-1_$(BOOST_VERSION_MAJOR)/" ;>> ../boost-version.jam
+cmd /A /C echo BOOST_VERSION_MAJOR = "%1" ;>> ..\boost-version.jam
+cmd /A /C echo BOOST_VERSION_MINOR = "%2" ;>> ..\boost-version.jam
+cmd /A /C echo BOOST_HEADERS = "../Boost/boost/include/boost-1_$(BOOST_VERSION_MAJOR)/" ;>> ..\boost-version.jam
+cmd /A /C echo BUILD_DIRECTORY = "../build.tmp/1_%1_%2" ;>> ..\boost-version.jam
