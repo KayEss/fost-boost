3b99aa2f404e6548b39c9ba609c2adb80bf53639
Kirit Sælensminde <k@kirit.com>
2009-05-13 12:10:11 +0000

    The install script now also creates a .cmd file to set the build directory to a relative path suitable for use by build scripts running in sibling folders.

--- install.cmd
+++ install.cmd
@@ -12,3 +12,5 @@ cmd /A /C echo BOOST_VERSION_MAJOR = "%1" ;>> ..\boost-version.jam
 cmd /A /C echo BOOST_VERSION_MINOR = "%2" ;>> ..\boost-version.jam
 cmd /A /C echo BOOST_HEADERS = "../Boost/boost/include/boost-1_$(BOOST_VERSION_MAJOR)/" ;>> ..\boost-version.jam
 cmd /A /C echo BUILD_DIRECTORY = "../build.tmp/1_%1_%2" ;>> ..\boost-version.jam
+
+cmd /A /C echo set BUILD_DIRECTORY=../build.tmp/1_%1_%2>> ..\boost-version.cmd
