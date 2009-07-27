0bf95a549a9b7f7cb8b1694a98fa12d20ef5a0a8
Kirit Sælensminde <k@kirit.com>
2009-07-27 13:53:33 +0000

    Updated the build options again.

--- compile.cmd
+++ compile.cmd
@@ -1,4 +1,4 @@
 cd %VERSION%
-bjam -j%NUMBER_OF_PROCESSORS% toolset=msvc-8.0 define=_SECURE_SCL=0 define=_CRT_SECURE_NO_DEPRECATE define=_CRT_SECURE_NO_WARNINGS --without-mpi tools asynch-exceptions=on exception-handling=on extern-c-nothrow=off
-bjam -j%NUMBER_OF_PROCESSORS% toolset=msvc-8.0 define=_SECURE_SCL=0 define=_CRT_SECURE_NO_DEPRECATE define=_CRT_SECURE_NO_WARNINGS --without-mpi debug release install link=shared threading=multi asynch-exceptions=on exception-handling=on extern-c-nothrow=off --prefix=..\boost
+bjam -j%NUMBER_OF_PROCESSORS% toolset=msvc-8.0 define=_SECURE_SCL=0 define=_CRT_SECURE_NO_DEPRECATE define=_CRT_SECURE_NO_WARNINGS --without-mpi release tools runtime-link=shared link=shared threading=multi asynch-exceptions=on exception-handling=on extern-c-nothrow=off
+bjam -j%NUMBER_OF_PROCESSORS% toolset=msvc-8.0 define=_SECURE_SCL=0 define=_CRT_SECURE_NO_DEPRECATE define=_CRT_SECURE_NO_WARNINGS --without-mpi debug release install runtime-link=shared link=shared threading=multi asynch-exceptions=on exception-handling=on extern-c-nothrow=off --prefix=..\boost
 cd ..
