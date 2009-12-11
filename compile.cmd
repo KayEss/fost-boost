064c20120e2594353d185f846aa8516ed52b8300
Kirit Sælensminde <k@kirit.com>
2009-12-11 07:31:51 +0000

    We don't need to build the tools.

--- compile.cmd
+++ compile.cmd
@@ -1,4 +1,4 @@
 cd %VERSION%
-bjam -j%NUMBER_OF_PROCESSORS% toolset=msvc define=_SECURE_SCL=0 define=_CRT_SECURE_NO_DEPRECATE define=_CRT_SECURE_NO_WARNINGS --without-mpi release tools runtime-link=shared link=shared threading=multi asynch-exceptions=on exception-handling=on extern-c-nothrow=off
+REM bjam -j%NUMBER_OF_PROCESSORS% toolset=msvc define=_SECURE_SCL=0 define=_CRT_SECURE_NO_DEPRECATE define=_CRT_SECURE_NO_WARNINGS --without-mpi release tools runtime-link=shared link=shared threading=multi asynch-exceptions=on exception-handling=on extern-c-nothrow=off
 bjam -j%NUMBER_OF_PROCESSORS% toolset=msvc define=_SECURE_SCL=0 define=_CRT_SECURE_NO_DEPRECATE define=_CRT_SECURE_NO_WARNINGS --without-mpi debug release install runtime-link=shared link=shared threading=multi asynch-exceptions=on exception-handling=on extern-c-nothrow=off --prefix=..\boost
 cd ..
