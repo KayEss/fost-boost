db415d5c72f04d2b3ad6fd398e0640a1f7e9c7b1
Kirit Sælensminde <k@kirit.com>
2010-02-02 05:16:12 +0000

    Don't try to be too clever about which things to build. Just do a compile even if it looks like Boost is already built.

--- build.cmd
+++ build.cmd
@@ -18,21 +18,18 @@ IF "%3"=="" (
 )
 
 SET VERSION=1_%MAJOR%_%MINOR%
-IF NOT EXIST "%LOCATION%\%VERSION%" (
-    IF NOT EXIST %VERSION% (
-        svn co http://svn.boost.org/svn/boost/tags/release/Boost_%VERSION% %VERSION%
-    ) ELSE (
-	    svn up %VERSION%
-    )
-    IF NOT EXIST %VERSION%\bjam.exe (
-        cd %VERSION%\tools\jam
-        call build_dist.bat
-        cd
-        copy bin.ntx86\bjam.exe ..\..\..
-        cd ..\..\..\..
-    )
-    call compile.cmd %VERSION%
+
+IF NOT EXIST %VERSION% (
+    svn co http://svn.boost.org/svn/boost/tags/release/Boost_%VERSION% %VERSION%
 )
+IF NOT EXIST %VERSION%\bjam.exe (
+    cd %VERSION%\tools\jam
+    call build_dist.bat
+    cd
+    copy bin.ntx86\bjam.exe ..\..\..
+    cd ..\..\..\..
+)
+call compile.cmd %VERSION%
 
 echo To use this version run:
 echo install.cmd %MAJOR% %MINOR% %LOCATION%
